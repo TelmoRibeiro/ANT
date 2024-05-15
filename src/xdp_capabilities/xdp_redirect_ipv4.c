31bb62f2a9a90b69cdf5a2f15c70f5b684a23500
TelmoRibeiro <telmoribeiro2706@gmail.com>
2024-05-15 20:47:32 +0100

    terrible business

--- src/xdp_capabilities/xdp_redirect_ipv4.c
+++ src/xdp_capabilities/xdp_redirect_ipv4.c
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>
 #include <linux/ip.h>
 
+#include <bpf/bpf_endian.h>
 #include <linux/if_packet.h>
 #include <linux/if_vlan.h>
 #include <linux/if.h>
@@ -13,48 +14,25 @@
 #include <linux/in6.h>
 #include <linux/types.h>
 
-#define IPV4_ADDR "192.168.2.0" // change me //
-#define IPV4_ETH_INDEX 2        // change me //
+#define ETH_INDEX 3
 
-/* @ telmo -
-    TESTED:
-    NOT TESTED:
-        - Generic Mode
-        - Native
-        - Offloaded
-    
-    IDEA:
-        - load xdp_redirect into an interface
-        - it will redirect the IPV6 packets to interface with index X
-*/
-
-/*
-struct bpf_map_def SEC("maps") MY_MAP = {
-    .type = BPF_MAP_TYPE_DEVMAP_HASH,
-    .key_size   = sizeof(__u32),
-    .value_size = sizeof(int),
-    .max_entries = 1024, 
-};
-*/
 SEC("xdp_redirect_ipv4")
-int xdp_redirect_prog(struct xdp_md* ctx) {
+int xdp_redirect_ipv4_prog(struct xdp_md* ctx) {
     void* data_end = (void*)(long)ctx->data_end;
     void* data     = (void*)(long)ctx->data;
     if (data + sizeof(struct ethhdr) > data_end) {
         return XDP_DROP;
     }
     struct ethhdr* eth = data;
-    if (eth->h_proto == htons(ETH_P_IPV6)) {
+    if (eth->h_proto != htons(ETH_P_IP)) {
         return XDP_PASS;
     }
+    struct iphdr* iph = data + sizeof(struct ethhdr);
     if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) > data_end) {
         return XDP_DROP;
     }
-    struct iphdr* iph = data + sizeof(struct ethhdr);
-    __u64 old_daddr = iph->daddr;
-    iph->daddr = inet_addr(IPV4_ADDR);
-    iph->check = bpf_csum_replace(x,y,old_daddr,iph->daddr);
-    // bpf_redirect_map(&MY_MAP,IPV4_ETH_INDEX,0);
-    return XDP_TX;
+    __u32 dst = inet_addr("192.168.1.95");
+    iph->daddr = dst;
+    return bpf_redirect(ETH_INDEX,0);
 }
 char _license[] SEC("license") = "GPL";
